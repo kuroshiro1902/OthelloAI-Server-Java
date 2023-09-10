3deeb8cadcb6e89767e9fa0acd61c2ed5de03dbd
newgate2601 <newgate2601@gmail.com>
2023-09-10 17:21:30 +0700

    feature: test git

--- src/main/java/com/example/Othello2/Othello2Application.java
+++ src/main/java/com/example/Othello2/Othello2Application.java
@@ -9,6 +9,7 @@ public class Othello2Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Othello2Application.class, args);
         System.out.println("phu2 test git");
+		System.out.println("phutest git 2");
 	}
 
 }
