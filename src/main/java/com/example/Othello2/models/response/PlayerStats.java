ef2921a6d8a7a1346ca63de94c5bdc715b716a59
kuroshiro1902 <emsinos1902@gmail.com>
2023-09-24 14:13:58 +0700

    add responses model

--- src/main/java/com/example/Othello2/models/response/PlayerStats.java
+++ src/main/java/com/example/Othello2/models/response/PlayerStats.java
@@ -6,9 +6,6 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.springframework.web.bind.annotation.RequestBody;
-
-import java.util.List;
 
 @Getter
 @Setter
