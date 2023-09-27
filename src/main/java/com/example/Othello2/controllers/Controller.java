4c0f724d431fbc35915f2b96e9b75c4a26c61022
newgate2601 <newgate2601@gmail.com>
2023-09-27 14:41:29 +0700

    fix: update Controller

--- src/main/java/com/example/Othello2/controllers/Controller.java
+++ src/main/java/com/example/Othello2/controllers/Controller.java
@@ -4,6 +4,8 @@ import com.example.Othello2.common.enums.Player;
 import com.example.Othello2.models.Cell;
 import com.example.Othello2.models.Move;
 import com.example.Othello2.GameServices.*;
+import com.example.Othello2.models.request.DynamicEvaluationRequest;
+import com.example.Othello2.models.request.FindValidMoveRequest;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.*;
@@ -24,8 +26,10 @@ public class Controller {
 
     @PostMapping("/dynamic-evaluation")
     @Operation(summary = "Đánh giá lợi thế")
-    public Double dynamicEvaluation(@RequestBody Cell[][] cells,@RequestParam String player,@RequestBody List<Move> validMovesForPlayer){
-        return dynamicEvaluationService.dynamicEvaluation(cells, Player.valueOf(player), validMovesForPlayer);
+    public Double dynamicEvaluation(@RequestBody DynamicEvaluationRequest request){
+        return dynamicEvaluationService.dynamicEvaluation(request.getCells(),
+                Player.valueOf(request.getPlayer()),
+                request.getValidMovesForPlayer());
     }
 
     @GetMapping("/initialize")
@@ -34,24 +38,9 @@ public class Controller {
         return initializeChessBoardService.initialize();
     }
 
-//    @Operation(summary = "Lấy tất cả quân cờ của người chơi hiện tại")
-//    @PostMapping("/all-discs-of-player")
-//    public List<Cell> getAllDiscsOfThisPlayer(@RequestBody Cell[][] cells,
-//                                              @RequestParam @NonNull String player){
-//        return getDiscService.getAllDiscsOfThisPlayer(cells, Player.valueOf(player));
-//    }
-
-    @PostMapping("/is-valid-move")
-    public Move isValidMove(@RequestBody Cell[][] cells,
-                            @RequestParam int row,
-                            @RequestParam int col,
-                            @RequestParam String player){
-        return isValidMovesService.isValidMoves(cells, row, col, Player.valueOf(player));
-    }
-
     @PostMapping("/find-valid-move")
-    List<Move> findValidMoves(@RequestBody Cell[][] cells, @RequestParam String player){
-        return findValidMoveService.findValidMoves(cells, Player.valueOf(player));
+    List<Move> findValidMoves(@RequestBody FindValidMoveRequest request){
+        return findValidMoveService.findValidMoves(request.getCells(), Player.valueOf(request.getPlayer()));
     }
 
 
